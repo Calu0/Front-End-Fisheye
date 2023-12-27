eb7285aa7b796e9525c587dfe3f320a555a86dc6
Luca <86166469+Calu0@users.noreply.github.com>
2023-12-27 14:01:14 +0100

    🎉  project-init

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,14 +1,17 @@
 function photographerTemplate(data) {
     const { name, portrait } = data;
 
+
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        article.setAttribute("class", "photographer_card")
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+
         article.appendChild(img);
         article.appendChild(h2);
         return (article);
